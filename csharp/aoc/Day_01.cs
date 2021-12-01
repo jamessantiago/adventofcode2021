aa39bdec95e25d23aec892c26016e4319435b8ec
jamessantiago <james.r.santiago@gmail.com>
2021-11-30 23:08:56 -0600

    day 1

--- csharp/aoc/Day_01.cs
+++ csharp/aoc/Day_01.cs
@@ -3,26 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace aoc
-{    public class Day_01 : BaseDay
+{    
+    public class Day_01 : BaseDay
     {
-        private readonly List<string> _input;
+        private readonly List<int> _input;
 
         public Day_01()
         {
-            _input = File.ReadLines(InputFilePath).ToList();
+            _input = File.ReadLines(InputFilePath).Select(d => int.Parse(d)).ToList();
         }
 
         public override ValueTask<string> Solve_1()
         {
-            return new("");
+            int increasing = 0;
+            for (int i = 1; i < _input.Count; i++)
+            {
+                if (_input[i] > _input[i - 1]) increasing++;
+            }
+            return new(increasing.ToString());
         }
 
         public override ValueTask<string> Solve_2()
         {
-            return new("");
+            int increasing = 0;
+            for (int i = 1; i < _input.Count - 2; i++)
+            {
+                int sum1 = _input[i - 1] + _input[i] + _input[i + 1];
+                int sum2 = _input[i] + _input[i + 1] + _input[i + 2];
+                if (sum2 > sum1) increasing++;
+            }
+            return new(increasing.ToString());
         }
     }
 }
