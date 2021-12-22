c3736b9c1411b5b87a7f6b4f64edfdd82199c04b
jamessantiago <james.r.santiago@gmail.com>
2021-12-22 07:57:09 -0600

    day

---
+++ csharp/aoc/Day_21.cs
@@ -0,0 +1,73 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc
+{
+    public class Day_21 : BaseDay
+    {
+        private readonly int[] _input;
+        private readonly int[] _input2;
+
+        public Day_21()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(d => Regex.Match(d, @"Player (\d+) starting position: (\d+)"))
+                .Select(d => int.Parse(d.Groups[2].Value) - 1).ToArray();
+            _input2 = (int[])_input.Clone();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            long[] scores = { 0, 0 };
+            long rolls = 0;
+            for (int i = 0; ;  i += 2 * 3)
+            {
+                _input[0] = (_input[0] + (i + 1 % 100) + (i + 2 % 100) + (i + 3 % 100)) % 10;
+                scores[0] += _input[0] + 1;
+                rolls = i + 3;
+                if (scores[0] >= 1000) break;
+                _input[1] = (_input[1] + (i + 4 % 100) + (i + 5 % 100) + (i + 6 % 100)) % 10;
+                scores[1] += _input[1] + 1;
+                rolls = i + 6;
+                if (scores[1] >= 1000) break;
+            }
+
+            return new((Math.Min(scores[0], scores[1]) * rolls).ToString());
+        }
+
+        public long[] wins = { 0, 0 };
+        public int[] probability = { 0, 0, 0, 1, 3, 6, 7, 6, 3, 1 };
+        public void getWins((int p1, int p2, int s1, int s2, long r, bool turn) state)
+        {
+            var (p1, p2, s1, s2, r, turn) = state;
+            if (turn)
+                for (int i = 3; i <= 9; i++)
+                {
+                    var np1 = (p1 + i) % 10;
+                    var ns1 = s1 + np1 + 1;
+                    if (ns1 >= 21) wins[0] += r * probability[i];
+                    else getWins((np1, p2, ns1, s2, r * probability[i], !turn));
+                }
+            else
+                for (int i = 3; i <= 9; i++)
+                {
+                    var np2 = (p2 + i) % 10;
+                    var ns2 = s2 + np2 + 1;
+                    if (ns2 >= 21) wins[1] += r * probability[i];
+                    else getWins((p1, np2, s1, ns2, r * probability[i], !turn));
+                }                
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            getWins((_input2[0], _input2[1], 0, 0, 1, true));
+
+            return new(wins.Max().ToString());
+        }
+    }
+}
---
+++ csharp/aoc/Day_22.cs
@@ -0,0 +1,135 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc
+{
+    public readonly record struct cube(int x1, int x2, int y1, int y2, int z1, int z2);
+
+    public static class util22
+    {
+        public static long area(this cube a) => (Math.Abs(a.x1 - a.x2) + 1) * (Math.Abs(a.y1 - a.y2) + 1) * (Math.Abs(a.z1 - a.z2) + 1);
+
+        public static bool contains(this cube a, cube b) =>
+            b.x1 > a.x2 && b.x2 < a.x2 &&
+            b.y1 > a.y2 && b.y2 < a.y2 &&
+            b.z1 > a.z2 && b.z2 < a.z2;
+
+        //public static bool intersects(this cube a, cube b) =>
+        //    !(a.x1 <= b.x1 || a.x1 >= b.x2 ||
+        //      a.y2 <= b.y1 || a.y1 >= b.y2 ||
+        //      a.z2 <= b.z1 || a.z1 >= b.z2);
+
+        public static bool intersects(this cube a, cube b) =>
+            Math.Min(a.x2, b.x2) >= Math.Max(a.x1, b.x1) &&
+            Math.Min(a.y2, b.y2) >= Math.Max(a.y1, b.y1) &&
+            Math.Min(a.z2, b.z2) >= Math.Max(a.z1, b.z1);
+
+        public static bool allowed(this cube a) =>
+            a.x1 >= -50 && a.x2 <= 50 &&
+            a.y1 >= -50 && a.y2 <= 50 &&
+            a.z1 >= -50 && a.z2 <= 50;
+
+        public static void AddOrUpdate(this Dictionary<cube, int> dict, cube key, int value)
+        {
+            if (dict.ContainsKey(key)) dict[key] += value;
+            else dict.Add(key, value);
+        }
+
+        public static void AddOrSet(this Dictionary<cube, int> dict, cube key, int value)
+        {
+            if (dict.ContainsKey(key)) dict[key] = value;
+            else dict.Add(key, value);
+        }
+    }
+
+    public class Day_22 : BaseDay
+    {
+        private readonly List<(bool on, cube cube)> _input;
+
+        public Day_22()
+        {
+            _input = File.ReadAllLines(InputFilePath)
+                .Select(d => Regex.Match(d, @"(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)"))
+                .Select(d => (
+                    d.Groups[1].Value == "on",
+                    new cube (
+                        int.Parse(d.Groups[2].Value), 
+                        int.Parse(d.Groups[3].Value),
+                        int.Parse(d.Groups[4].Value),
+                        int.Parse(d.Groups[5].Value),
+                        int.Parse(d.Groups[6].Value),
+                        int.Parse(d.Groups[7].Value)
+                        )
+                    )
+                ).ToList();
+        }
+
+        public cube Intersect(cube a, cube b)
+        {
+            var c = new cube(
+                Math.Max(a.x1, b.x1),
+                Math.Min(a.x2, b.x2),
+                Math.Max(a.y1, b.y1),
+                Math.Min(a.y2, b.y2),
+                Math.Max(a.z1, b.z1),
+                Math.Min(a.z2, b.z2)
+                );
+
+            return c;
+        }
+
+
+        public ValueTask<string> Solve(bool all)
+        {
+            var finalCubes = new Dictionary<cube, int>();
+            var allowed = _input.Where(d => all || d.cube.allowed());
+            foreach (var (on, c) in allowed)
+            {
+                var temp = new Dictionary<cube, int>();
+                foreach (var fc in finalCubes.Keys)
+                {
+                    if (fc.intersects(c))
+                    {
+                        var newc = Intersect(fc, c);
+                        //Console.WriteLine($"{c.GetHashCode(),-12} intersects with {fc.GetHashCode()}");
+                        temp.AddOrUpdate(newc, -finalCubes[fc]);
+                    }
+                }
+                if (on) temp.AddOrUpdate(c, 1);
+                foreach (var tc in temp)
+                {
+                    finalCubes.AddOrUpdate(tc.Key, tc.Value);
+                    //Console.WriteLine($"{tc.Key.GetHashCode(),-12} intersection/cube now {finalCubes[tc.Key]}");
+                }
+                //Console.WriteLine($"{temp.Count,-4} intersections, total on: {finalCubes.Sum(d =>  d.Key.area() * d.Value)}");
+                //Console.WriteLine();
+            }
+
+            var result = 0L;
+            foreach (var fc in finalCubes)
+            {
+                result = checked(result + fc.Value * fc.Key.area());
+            }
+
+            return new(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return Solve(false);
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return Solve(true);
+        }
+    }
+
+
+}
